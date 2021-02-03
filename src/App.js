7315af2a069ca3f63b5e76bc647da551418c765b
Jason Tran <jaycehuytran@gmail.com>
2021-02-03 15:37:41 -0800

    edit particle values

--- src/App.js
+++ src/App.js
@@ -14,15 +14,16 @@ class App extends Component {
   render() {
     const particlesOptions = {
       particles: {
-        line_linked: {
-          shadow: {
-            enable: true,
-            color: "#3CA9D1",
-            blur: 5
+          number: {
+            value: 50,
+            density: {
+              enable: true,
+              value_area: 800
           }
         }
       }
     }
+    
 
     return (
       <div className = "App">
