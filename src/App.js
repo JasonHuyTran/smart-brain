704b0153ea544506153a55d8af257829992e777c
Jason Tran <jaycehuytran@gmail.com>
2021-02-03 15:33:50 -0800

    added facerecognition, imagelinkform, logo, and rank componnts. And added particles in the background

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
 import React, {Component} from 'react';
 import './App.css';
 import 'tachyons'
+import Particles from 'react-particles-js';
+ 
 
 import Navigation from "./components/Navigation/Navigation.components"
 import Logo from "./components/Logo/Logo.components"
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.components"
+import Rank from "./components/Rank/Rank.component"
 
 
 class App extends Component {
   render() {
+    const particlesOptions = {
+      particles: {
+        line_linked: {
+          shadow: {
+            enable: true,
+            color: "#3CA9D1",
+            blur: 5
+          }
+        }
+      }
+    }
+
     return (
       <div className = "App">
+        <Particles className = "particles"
+              params={particlesOptions}
+            />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
