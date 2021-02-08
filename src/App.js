61053f39fb00a735385972abd2cab283ee43ff2c
Jason Tran <jaycehuytran@gmail.com>
2021-02-08 01:47:47 -0800

    added API request in register

--- src/App.js
+++ src/App.js
@@ -24,9 +24,26 @@ class App extends Component {
       imageUrl: '',
       boxes: [],
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
+
+  loadUser = ((data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  });
 
   calculateFaceLocation = (data) => {
     const arrOfFacesLocation = data.outputs[0].data.regions;
@@ -109,7 +126,7 @@ class App extends Component {
           : (
             route === 'signin'
             ? <Signin onRouteChange = {this.onRouteChange}/>
-            : <Register onRouteChange = {this.onRouteChange}/>
+            : <Register onRouteChange = {this.onRouteChange} loadUser = {this.loadUser}/>
           )
         }
 
